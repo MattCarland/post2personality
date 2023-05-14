f2a28c01d4e2aa2da289f1a0bdbf8e05a59518fa
Éric Férole <eferole@pixelcircus.ca>
2023-05-14 12:27:31 -0400

    Added data

---
+++ data.py
@@ -0,0 +1,12 @@
+class Data:
+    def get_data(self):
+        """
+        This function returns a Python dict.
+        Its keys should be 'set01', 'set02', 'set03' etc...
+        Its values should be pandas.DataFrames loaded from csv files
+        """
+        # Hints 1: Build csv_path as "absolute path" in order to call this method from anywhere.
+            # Do not hardcode your path as it only works on your machine ('Users/username/code...')
+            # Use __file__ instead as an absolute path anchor independant of your usename
+            # Make extensive use of `breakpoint()` to investigate what `__file__` variable is really
+        # Hint 2: Use os.path library to construct path independent of Mac vs. Unix vs. Windows specificities
