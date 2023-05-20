184a642a4990448d61b9aea9d765f1c7ad3a0313
Andrew Forbes <aforbesj@gmail.com>
2023-05-20 17:24:50 -0400

    All files for today, Pull the trigger on the model choice

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,4 +1,4 @@
-from preprocessing-functions import *
+from scripts.preprocessing_functions import *
 
 
 def preprocessing(data):
@@ -6,37 +6,28 @@ def preprocessing(data):
     # remove URLs
     remove_urls(data)
 
-
     # remove social media handles
     remove_handles(data)
-
 
     # remove punctuation
     punctuation(data)
 
-
     # lowercase
     lower_case(data)
-
 
     # remove special characters
     remove_special_characters(data)
 
-
     # remove white space
     white_space(data)
-
 
     # tokenizing
     tokenize(data)
 
-
     # stopword removal
     stopwords(data)
-
 
     # text lemmatization
     lemmatize(data)
 
-
-return data
+    return data
