3a7391e9c9491f55df293f95b83df3f086cee5d2
mohammad auda <moe93@live.com>
2023-06-03 13:05:36 -0400

    added params.py and registry.py

---
+++ model/params.py
@@ -0,0 +1,49 @@
+import os
+import numpy as np
+
+##################  VARIABLES  ##################
+DATA_SIZE = os.environ.get("DATA_SIZE")
+# CHUNK_SIZE=200
+
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+GCP_PROJECT = os.environ.get("GCP_PROJECT")
+GCP_PROJECT_WAGON = os.environ.get("GCP_PROJECT_WAGON")
+GCP_REGION = os.environ.get("GCP_REGION")
+BQ_DATASET = os.environ.get("BQ_DATASET")
+BQ_REGION = os.environ.get("BQ_REGION")
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+INSTANCE = os.environ.get("INSTANCE")
+
+
+
+##################  CONSTANTS  #####################
+LOCAL_DATA_PATH = os.path.join(os.path.expanduser('~'), ".lewagon", "mlops", "data")
+LOCAL_REGISTRY_PATH =  os.path.join(os.path.expanduser('~'), ".lewagon", "mlops", "training_outputs")
+
+COLUMN_NAMES_RAW = ['type', 'text', 'avg_word_length', 'type_to_token_ratio']
+
+DTYPES_RAW = {
+    "type": "object",
+    "text": "object",
+    "avg_word_length": "float32",
+    "type_to_token_ratio": "float32"
+}
+
+DTYPES_PROCESSED = np.float32
+
+
+################## VALIDATIONS #################
+
+env_valid_options = dict(
+    DATA_SIZE=["1k", "200k", "all"],
+    MODEL_TARGET=["local", "gcs", "mlflow"],
+)
+
+def validate_env_value(env, valid_options):
+    env_value = os.environ[env]
+    if env_value not in valid_options:
+        raise NameError(f"Invalid value for {env} in `.env` file: {env_value} must be in {valid_options}")
+
+
+for env, valid_options in env_valid_options.items():
+    validate_env_value(env, valid_options)
